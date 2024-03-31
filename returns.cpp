80e361c6b955978da431ca062f091ce722acac8a
furionias <82088657+furionias@users.noreply.github.com>
2024-03-31 14:22:17 +0000

    d

--- returns.cpp
+++ returns.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 
-const int ui = () => {
-    return 67;
-}
 double square(double length);
 double cube(double length);
-
+std::string concatStrings(std::string string1, std::string string2);
 int main(){
    
     double length = 5.0;
     double area = square(length);
-    int y = ui();
-    double cube1 = cube(length)
+    double cube1 = cube(length);
     std::cout << "Area: " << area << "cm^2\n";
-    std::cout << y;
     std::cout << "Area (cubed): " << cube << "cm^3\n";
+
+    std::string first_name = "Asriel";
+    std::string last_name = "Ezeife";
+    
+    std::string full_name = concatStrings(first_name, last_name);
+    std::cout << full_name;
+
 
     return 0;
 }
@@ -24,5 +26,9 @@ double square(double length){
 }
 double cube(double length){
     double result = length * length * length;
-    return result
+    return result;
+}
+
+std::string concatStrings (std::string string1, std::string string2){
+    return string1 + " " + string2;
 }
