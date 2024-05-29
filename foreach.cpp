d557273f7d6888d81e753a5109f94b50ce331378
furionias <82088657+furionias@users.noreply.github.com>
2024-05-29 16:19:56 +0000

    s

---
+++ foreach.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    //foreach loop = loop that eases the traversal over an iterable data type
+
+    char names[] = {'A', 'B', 'C', 'D'};
+    for(std::string name : names){
+        std::cout << i;
+    }
+    return 0;
+
+}
---
+++ iterarr.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    std::string names[] = {"Asriel", "w", "w"};
+    char names[] = {"A", "B", "C", "D"}
+    for (int i =0; i<sizeof(students); i++;){
+        std::cout << students[i] << "/n";
+    }
+    return 0;
+}
