b5328ec7a9757eb98957e936fcbe01e936cfc748
furionias <82088657+furionias@users.noreply.github.com>
2024-06-03 14:30:43 +0000

    s

--- sortarr.cpp
+++ sortarr.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
+void sort(int array[], int size);
 int main(){
-    int array[] = {10, 20, 30, 67, 12,23};
+    int array[] = {100, 20, 30, 67, 12,23};
     int size = sizeof(array)/ sizeof(array[0]);
     
+    sort(array, size);
+
+    for (int element : array){
+        std::cout << element;
+    }
+  
+    
+}
+void sort(int array[], int size){
+    int temp;
+    for (int i = 0; i < size -1; i++){
+        for (int j = 0;  j < size - i -1; j++){
+            if (array[j] > array[j+1]){
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+
 }
